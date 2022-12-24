dd9cecea813e8b535e9377d4eec4321c0f197cc0
Yusuke.k <102857514+Uchan-code@users.noreply.github.com>
2022-12-24 16:26:50 +0900

    section01
    JSONの作成,console.log("load"),cssの読み込みをしよう

---
+++ Section1/scripts/content.js
@@ -0,0 +1,10 @@
+// TODO01 create "manifest.json"
+// TODO02 import "content.js" and output "load"
+// TODO03 push HTML
+
+const tag = document.createElement("h3")
+tag.textContent = "textextext"
+const body = document.querySelector("body")
+body.appendChild(tag)
+
+  console.log("load");
