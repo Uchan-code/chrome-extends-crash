8478b54a2d5c7ce56b135c241472d671d499e43e
Yusuke.k <102857514+Uchan-code@users.noreply.github.com>
2022-12-26 07:51:17 +0900

    2022-12-26

---
+++ start/popup/popup.js
@@ -0,0 +1,49 @@
+const tasks = [];
+
+const addTaskBtn = document.getElementById("add-task-btn");
+addTaskBtn.addEventListener("click", () => addTask());
+
+function renderTask(taskNum) {
+    const taskflow = document.createElement("div");
+
+    const text = document.createElement("input")
+    text.type = "text";
+    text.placeholder = "Enter a task";
+    text.value = tasks[taskNum]
+    text.addEventListener("change", () => {
+        tasks[tasksNum] = text.value
+        console.log(tasks)
+    })
+
+    const deleteBtn = document.createElement("input");
+    deleteBtn.type = "button";
+    deleteBtn.value = "X";
+    deleteBtn.addEventListener("click", () => {
+        deleteTask(taskNum);
+    })
+
+    taskflow.appendChild(text);
+    taskflow.appendChild(deleteBtn);
+
+    const taskContainer = document.getElementById("task-container");
+    taskContainer.appendChild(taskflow);
+}
+
+function addTask() {
+    const tasksNum = tasks.length
+    tasks.push("");
+    renderTask(tasksNum)
+}
+
+function deleteTask(taskNum) {
+    tasks.splice(taskNum, 1)
+    renderTasks()
+}
+
+function renderTasks() {
+    const taskContaner = document.getElementById("task-container");
+    taskContaner.textContent = "";
+    tasks.forEach((taskText, taskNum) => {
+        renderTask(taskNum)
+    })
+}
