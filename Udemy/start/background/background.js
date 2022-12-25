a1d2e55f7c7f95a278c43a490794a34592d6a69a
Yusuke.k <102857514+Uchan-code@users.noreply.github.com>
2022-12-25 13:22:52 +0900

    Add files via upload

---
+++ Udemy/start/background/background.js
@@ -0,0 +1,29 @@
+console.log("Hello from the background JS")
+
+chrome.alarms.create({
+    periodInMinutes: 1 / 60,
+});
+
+chrome.alarms.onAlarm.addListener((alarm) => {
+    chrome.storage.local.get(["timer"], (res) => {
+        const time = res.timer ?? 0;
+        chrome.storage.local.set({
+            timer: time + 1
+        })
+        chrome.action.setBadgeText({
+            text: `${time + 1}`
+        })
+        chrome.storage.sync.get(["notificationTime"], (res) => {
+            const notificationTime = res.notificationTime ?? 1200
+            if (time % notificationTime == 0) {
+                this.registration.showNotification("Chrome Timer Extension", {
+                    body: `${notificationTime} second has passed`,
+                    icon: "icon.png"
+                }
+                )
+            }
+        })
+    })
+})
+
+console.log(this)
