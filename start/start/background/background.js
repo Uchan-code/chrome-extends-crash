29b16599e6e9dfaa4a3a7d760f347b45115d1e73
Yusuke.k <102857514+Uchan-code@users.noreply.github.com>
2022-12-28 16:22:04 +0900

    2022-12-28

--- start/start/background/background.js
+++ start/start/background/background.js
@@ -1,13 +1,32 @@
 chrome.alarms.create("pomodoroTimer", {
-    periodMinutes: 1 / 60,
+    periodInMinutes: 1 / 60,
 })
 
-chrome.alarms.onAlarm.addEventlistener((alarm) => {
+chrome.alarms.onAlarm.addListener((alarm) => {
     if (alarm.name === "pomodoroTimer") {
-
+        chrome.storage.local.get(["timer", "isRunning"], (res) => {
+            if (res.isRunning) {
+                let timer = res.timer + 1
+                let isRunning = true
+                if (timer == 60 * 25) {
+                    chrome.registration.showNotification("Pomodoro Timer", {
+                        body: "25 minutes has passed!",
+                        icon: "icon.png",
+                    })
+                    timer = 0;
+                    isRunning = false
+                }
+                chrome.storage.local.set({
+                    timer,
+                })
+            }
+        })
     }
 })
 
-chrome.storage.local.get([
-
-])
+chrome.storage.local.get(["timer", "isRunning"], (res) => {
+    chrome.storage.local.set({
+        timer: "timer" in res ? res.timer : 0,
+        isRunning: "isRunning" in res ? res.isRunning : false,
+    })
+})
