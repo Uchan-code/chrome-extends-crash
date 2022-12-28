293b7a18ca9eb1366b7dd6516880a099a3889b51
Yusuke.k <102857514+Uchan-code@users.noreply.github.com>
2022-12-28 22:56:05 +0900

    2022-12-28

--- start/popup/popup.js
+++ start/popup/popup.js
@@ -1,25 +1,73 @@
-const tasks = [];
+let tasks = [];
+
+function updateTime() {
+    chrome.storage.local.get(["timer", "timeOption"], (res) => {
+        const timer = document.getElementById("time")
+        const minutes = `${res.timeOption - Math.ceil(res.timer / 60)}`.padStart(2)
+        let seconds = "00";
+        if (res.timer % 60 != 0) {
+            seconds = `${60 - res.timer % 60}`.padStart(2)
+        }
+        timer.textContent = `${minutes}:${seconds}`
+    })
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+const startTimerBtn = document.getElementById("start-timer-btn")
+startTimerBtn.addEventListener("click", () => {
+    chrome.storage.local.get(["isRunning"], (res) => {
+        chrome.storage.local.set({
+            isRunning: !res.isRunning,
+        }, () => {
+            startTimerBtn.textContent = !res.isRunning ? "Pause Timer" : "Start Timer"
+        })
+    })
+})
+
+const resetTimerBtn = document.getElementById("reset-timer-btn");
+resetTimerBtn.addEventListener("click", () => {
+    chrome.storage.local.set({
+        timer: 0,
+        isRunning: false,
+    }, () => {
+        startTimerBtn.textContent = "Start Timer"
+    })
+})
 
 const addTaskBtn = document.getElementById("add-task-btn");
 addTaskBtn.addEventListener("click", () => addTask());
 
-function renderTask(taskNum) {
+chrome.storage.sync.get(["tasks"], (res) => {
+    tasks = res.tasks ? res.tasks : [];
+    renderTask();
+})
+
+function saveTasks() {
+    chrome.storage.sync.set({
+        tasks,
+    })
+}
+
+function renderTask(tasksNum) {
     const taskflow = document.createElement("div");
 
     const text = document.createElement("input")
     text.type = "text";
     text.placeholder = "Enter a task";
-    text.value = tasks[taskNum]
+    text.value = tasks[tasksNum];
+    text.className = "task-input"
     text.addEventListener("change", () => {
         tasks[tasksNum] = text.value
-        console.log(tasks)
+        saveTasks();
     })
 
     const deleteBtn = document.createElement("input");
     deleteBtn.type = "button";
     deleteBtn.value = "X";
     deleteBtn.addEventListener("click", () => {
-        deleteTask(taskNum);
+        deleteTask(tasksNum);
     })
 
     taskflow.appendChild(text);
@@ -33,17 +81,19 @@ function addTask() {
     const tasksNum = tasks.length
     tasks.push("");
     renderTask(tasksNum)
+    saveTasks();
 }
 
-function deleteTask(taskNum) {
-    tasks.splice(taskNum, 1)
+function deleteTask(tasksNum) {
+    tasks.splice(tasksNum, 1)
     renderTasks()
+    saveTasks();
 }
 
 function renderTasks() {
     const taskContaner = document.getElementById("task-container");
     taskContaner.textContent = "";
-    tasks.forEach((taskText, taskNum) => {
-        renderTask(taskNum)
+    tasks.forEach((taskText, tasksNum) => {
+        renderTask(tasksNum)
     })
 }
