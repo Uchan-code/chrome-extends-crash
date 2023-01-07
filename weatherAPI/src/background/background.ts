66f81f7d270769ebb7e8662f6db2a0b915ccbe66
Uchan <shocking.white.0103@gmail.com>
2023-01-07 11:00:35 +0900

    2023-01-07

--- weatherAPI/src/background/background.ts
+++ weatherAPI/src/background/background.ts
@@ -1,4 +1,5 @@
-import { setStoragedCities,setStoredOptions } from "../utills/storage";
+import { getStoredCities,getStoredOptions,setStoragedCities,setStoredOptions } from "../utills/storage";
+import { fetchOpenWeatherData } from "../utills/api";
 
 chrome.runtime.onInstalled.addListener(() => {
   setStoragedCities([])
@@ -6,5 +7,36 @@ chrome.runtime.onInstalled.addListener(() => {
     hasAutoOverlay: false,
     homeCity: "",
     tempScale: "metric"
+  })
+
+  chrome.contextMenus.create({
+    contexts: ["selection"],
+    title: " Add city to weather extension ",
+    id: "weatherExtension",
+  })
+
+  chrome.alarms.create({
+      periodInMinutes: 60,
+  })
+})
+
+chrome.alarms.onAlarm.addListener(() => {
+  getStoredOptions().then((options) => {
+    if(options.homeCity == ""){
+      return 
+    }
+    fetchOpenWeatherData(options.homeCity,options.tempScale).then((data) => {
+      const temp = Math.round(data.main.temp)
+      const symbol = options.tempScale == "metric" ? "\u2103" : "\u2109"
+      chrome.action.setBadgeText({
+        text: `${temp}${symbol}`,
+      })
+    })
+  })
+})
+
+chrome.contextMenus.onClicked.addListener((event) => {
+  getStoredCities().then((cities) => {
+    setStoragedCities([...cities, event.selectionText])
   })
 })
