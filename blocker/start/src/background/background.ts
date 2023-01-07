3207688571e96a0a76130bfdb8d8e1f5d849c78d
Uchan <shocking.white.0103@gmail.com>
2023-01-07 21:00:44 +0900

    2023-01-07

--- blocker/start/src/background/background.ts
+++ blocker/start/src/background/background.ts
@@ -1,4 +1,13 @@
 // TODO: background script
-chrome.runtime.onInstalled.addListener(() => {
-  // TODO: on installed function
-})
+// chrome.runtime.onInstalled.addListener(() => {
+//   // TODO: on installed function
+// })
+
+chrome.webRequest.onBeforeRequest.addListener((details) => {
+  console.log(details)
+  return {
+    cancel: true,
+  }
+},{
+  urls: [`*;//*.googleadservices.com/*`],
+},["blocking"])
