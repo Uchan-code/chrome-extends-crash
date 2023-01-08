cd37ff87810d1a4ac3da76b5d8c2a724ba9f408f
Uchan <shocking.white.0103@gmail.com>
2023-01-08 18:18:52 +0900

    2023-01-08

--- blocker/start/src/background/background.ts
+++ blocker/start/src/background/background.ts
@@ -3,11 +3,21 @@
 //   // TODO: on installed function
 // })
 
-chrome.webRequest.onBeforeRequest.addListener((details) => {
-  console.log(details)
-  return {
-    cancel: true,
-  }
-},{
-  urls: [`*;//*.googleadservices.com/*`],
-},["blocking"])
+// chrome.webRequest.onBeforeRequest.addListener((details) => {
+//   const url = details.url
+//   const filters = ["googleadservices", "googlesyndication", "g.doubleclick"]
+//   for(const filter of filters){
+//     if(url.indexOf(filter) != -1){
+//       return {
+//         cancel: true,
+//       }
+//     }
+//   }
+// },{
+//   urls: [
+//     "<all_urls>",
+//     `*://*.googleadservices.com/*`,
+//     `*://*.tpc.googlesyndication.com/*`,
+//     "*://.g.doubleclick.net/*"
+//   ],
+// },["blocking"])
